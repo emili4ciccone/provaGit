f16f29db6991515bb7152d07bf6ba1135dd8edb2
emili4ciccone <emiliaciccone@gmail.com>
2022-10-03 15:48:45 +0200

    Sei su Git -- modificato

--- git.provaGit/src/git/provaGit/GitTest.java
+++ git.provaGit/src/git/provaGit/GitTest.java
@@ -2,6 +2,6 @@ package git.provaGit;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Sei su Git");
+		System.out.println("Sei su Git -- modificato");
 	}
 }
