467020bb5e7dc357601ada3c83cb14ec34dbbe08
emili4ciccone <emiliaciccone@gmail.com>
2022-10-03 16:05:36 +0200

    modificato

--- git.provaGit/src/git/provaGit/GitTest.java
+++ git.provaGit/src/git/provaGit/GitTest.java
@@ -2,6 +2,6 @@ package git.provaGit;
 
 public class GitTest {
 	public static void main(String[] args) {
-		System.out.println("Sei su Git -- modificato 3");
+		System.out.println("Sei su Git -- modificato 4");
 	}
 }
