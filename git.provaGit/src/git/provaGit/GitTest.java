ffcf099b2ff01d6d007cd7c4fe15a1ac77479a5f
emili4ciccone <emiliaciccone@gmail.com>
2022-10-03 17:34:48 +0200

    Commit da IntelliJ numero 2

--- git.provaGit/src/git/provaGit/GitTest.java
+++ git.provaGit/src/git/provaGit/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("Sei su Git -- modificato");
 		System.out.println("PFFFF");
+		System.out.println("Modificato da emilia");
 	}
 }
